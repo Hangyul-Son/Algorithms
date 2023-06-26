3e5c474b9ee5b911ae424f05f5b34e0363e5ef9d
Han <hangyulson1004@gmail.com>
2023-06-26 23:13:03 +0800

    25. Array Manipulation

---
+++ 25. Array Manipulation/optimized.py
@@ -0,0 +1,54 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'arrayManipulation' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY queries
+#
+
+def arrayManipulation(n, queries):
+    # Write your code here
+    arr = [0] * n
+    for q in queries:
+        start, end, val = q
+        arr[start-1] = arr[start-1] + val
+        if end < n:
+            arr[end] = arr[end] - val
+    
+    current = 0
+    max_val = 0
+    for i in arr:
+        current += i
+        if current > max_val:
+            max_val = current    
+    
+    return max_val
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    queries = []
+
+    for _ in range(m):
+        queries.append(list(map(int, input().rstrip().split())))
+
+    result = arrayManipulation(n, queries)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
